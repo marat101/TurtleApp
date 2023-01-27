edb68b7c3b1330f6214ca674a7818422dd83d7a1
marat <maratnv101@gmail.com>
2023-01-27 20:22:51 +0300

    Заменил базу данных room на sqldelight

--- turtle_database/build/generated/sqldelight/code/TurtleDatabase/com/turtleteam/turtledatabase/GroupsDaysList.kt
+++ turtle_database/build/generated/sqldelight/code/TurtleDatabase/com/turtleteam/turtledatabase/GroupsDaysList.kt
@@ -1,10 +1,12 @@
 package com.turtleteam.turtledatabase
 
+import kotlin.String
+
 public data class GroupsDaysList(
-    public val days: String,
-    public val name: String
+  public val days: String,
+  public val name: String
 ) {
-    public override fun toString(): String = """
+  public override fun toString(): String = """
   |GroupsDaysList [
   |  days: $days
   |  name: $name
--- turtle_database/build/generated/sqldelight/code/TurtleDatabase/com/turtleteam/turtledatabase/TeachersDaysList.kt
+++ turtle_database/build/generated/sqldelight/code/TurtleDatabase/com/turtleteam/turtledatabase/TeachersDaysList.kt
@@ -1,10 +1,12 @@
 package com.turtleteam.turtledatabase
 
+import kotlin.String
+
 public data class TeachersDaysList(
-    public val days: String,
-    public val name: String
+  public val days: String,
+  public val name: String
 ) {
-    public override fun toString(): String = """
+  public override fun toString(): String = """
   |TeachersDaysList [
   |  days: $days
   |  name: $name
