eaee1a9d9d666fc502a89131848e62dc2fdd6b63
crazy <crazyeden@yandex.ru>
2023-01-31 20:54:04 +0300

    add icons to MoreScreen

--- ui/src/main/java/com/turtleteam/ui/TextWithFont.kt
+++ ui/src/main/java/com/turtleteam/ui/TextWithFont.kt
@@ -5,10 +5,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import com.turtleteam.ui.theme.fontFamily
 
 @Composable
-fun TextWithFont(modifier: Modifier = Modifier,text:String,color: Color,textSize: TextUnit){
-    Text(modifier = modifier,text = text, fontFamily = fontFamily, color = color, fontSize = textSize)
+fun TextWithFont(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color,
+    textSize: TextUnit,
+    align: TextAlign = TextAlign.Start
+){
+    Text(
+        modifier = modifier,
+        text = text,
+        fontFamily = fontFamily,
+        color = color,
+        fontSize = textSize,
+        textAlign = align
+    )
 }
--- ui/src/main/java/com/turtleteam/ui/screens/morescreen/MoreScreen.kt
+++ ui/src/main/java/com/turtleteam/ui/screens/morescreen/MoreScreen.kt
@@ -1,10 +1,76 @@
 package com.turtleteam.ui.screens.morescreen
 
-import androidx.compose.material.Text
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.turtleteam.ui.R
+import com.turtleteam.ui.TextWithFont
+import com.turtleteam.ui.theme.JetTheme
 
 @Composable
 fun MoreScreen(navController: NavHostController) {
-    Text(text = "MoreScreen")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row {
+            MoreScreenOneItem(
+                drawableId = R.drawable.ic_call_schedule,
+                stringResource(R.string.schedule_list)
+            ) {
+                //todo click
+            }
+            MoreScreenOneItem(
+                drawableId = R.drawable.ic_googlesheets,
+                stringResource(R.string.tablet)
+            ) {
+                //todo click
+            }
+        }
+        MoreScreenOneItem(
+            drawableId = R.drawable.ic_turtle_team,
+            stringResource(R.string.turtle_team)
+        ) {
+            //todo click
+        }
+    }
+}
+
+@Composable
+fun MoreScreenOneItem(
+    @DrawableRes drawableId: Int,
+    text: String,
+    onClickAction: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .size(130.dp)
+            .clickable(onClick = onClickAction)
+            .padding(10.dp)
+            .background(JetTheme.color.transparentBackground, RoundedCornerShape(8.dp))
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier.weight(1f),
+            painter = painterResource(id = drawableId),
+            contentDescription = null
+        )
+        TextWithFont(text = text, color = Color.Gray, textSize = 12.sp, align = TextAlign.Center)
+    }
 }
