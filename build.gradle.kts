121eff9fbd687fea3d9d7a6e16d490d8462c7d22
marat.navruzaliev <maratnv101@gmail.com>
2023-03-27 16:47:09 +0300

    upd

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ buildscript {
 
     dependencies {
         classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
+        classpath("com.google.gms:google-services:4.3.15")
     }
     repositories {
         mavenCentral()
